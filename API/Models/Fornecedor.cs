100c15edcd3f4dbf2e778b6f0b61d917ef66dab2
Felipe Silva <lipe1398@gmail.com>
2022-09-28 21:19:58 -0300

    Model - fornecedor

--- API/Models/Fornecedor.cs
+++ API/Models/Fornecedor.cs
@@ -3,5 +3,17 @@ using System;
 namespace API
 
 {
+    public class Fornecedor
+    {
+        public Fornecedor() => CriadoEm = DateTime.Now;
+
+        public int FornecedorId { get; set; }
+
+        public string NomeFornecedor { get; set; }
+
+        public string CnpjFornecedor { get; set; }
+
+        public string EmailFornecedor { get; set; }
+
 
 }
