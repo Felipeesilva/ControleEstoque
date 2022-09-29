0362cf463f089eea1236bf4c6f94032bd4bdc734
mateuslfr <82336133+mateuslfr@users.noreply.github.com>
2022-09-29 19:15:50 -0300

    estoque

--- API/Models/Estoque.cs
+++ API/Models/Estoque.cs
@@ -3,5 +3,15 @@ using System;
 namespace API
 
 {
+    public class Estoque
+    {
+        public Estoque() => CriadoEm = DateTime.Now;
 
+        public string Produto { get; set; } 
+
+        public int QuantidadeProduto { get; set; }
+
+        public int ValorProduto { get; set; }
+        
+     }
 }
