631abc8108623b038eaf947044dfa2637eec030a
PEDRO NETO <13161874978@educacional.up>
2022-09-29 21:41:13 -0300

    Atualização do estoque Controller e Model estoque

--- API/Controllers/EstoqueController.cs
+++ API/Controllers/EstoqueController.cs
@@ -1,7 +1,39 @@
-namespace API
+using System.Linq;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
 {
-    public class EstoqueController
+    [ApiController]
+    [Route("api/estoque")]
+    public class EstoqueController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+        public EstoqueController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //POST: api/estoque/create
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] Estoque estoque)
+        {
+            // Estoque estoqueEncontrado = _context.Estoques.FirstOrDefault(e => e.TipoEstoque == estoque.TipoEstoque);
+
+            // if(estoqueEncontrado == null)
+            // {
+            _context.Estoques.Add(estoque);
+            _context.SaveChanges();
+            return Created("", estoque);
+            // }
+            // return NotFound();
+        }
+
+        //GET: api/estoque/list
+        [HttpGet]
+        [Route("list")]
+        public IActionResult List() => Ok(_context.Estoques.ToList());
+
     }
 }
--- API/Models/Estoque.cs
+++ API/Models/Estoque.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API
 
@@ -7,11 +8,19 @@ namespace API
     {
         public Estoque() => CriadoEm = DateTime.Now;
 
-        public string Produto { get; set; } 
+        [Key()]
+        public string EstoqueId { get; set; } 
 
+        public string ProdutoId { get; set; } 
+        public virtual Produto produto { get; set; } 
+        
         public int QuantidadeProduto { get; set; }
 
         public int ValorProduto { get; set; }
+
+        public DateTime CriadoEm { get; set; }
+        
+    
         
      }
 }
