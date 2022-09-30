722ea9f3460b374de66356917292ce38b5c998f3
mateuslfr <82336133+mateuslfr@users.noreply.github.com>
2022-09-29 21:31:47 -0300

    produtoController

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -1,4 +1,11 @@
-namespace API
+using System.Linq;
+using System.Collections.Generic;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers
 {
     [ApiController]
     [Route("api/produto")]
@@ -9,12 +16,71 @@ namespace API
         {
             _context = context;
         }
-    
-     //POST: api/produto/criar
+
+        private static List<Produto> produtos = new List<Produto>();
+
+        //POST: api/produto/create
+        [Route("create")]
         [HttpPost]
-        [Route("criar")]
-        public IActionResult criar([FromBody] Produto produto)
+
+        public IActionResult Create([FromBody] Produto produto)
+        {
+            Produto produtoEncontrado = _context.Produtos.FirstOrDefault(p => p.NomeProduto == produto.NomeProduto);
+            if (produtoEncontrado == null)
+
+                _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return Created("", produto);
+
+        }
+
+        //GET: api/produto/list
+        [Route("list")]
+        [HttpGet]
+        public IActionResult List() => Ok(_context.Produtos.ToList());
+
+        //GET: api/produto/getbyid/1
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Produto produto = _context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        //DELETE: /api/produto/delete/name
+        [HttpDelete]
+        [Route("delete/{name}")]
+        public IActionResult Delete([FromRoute] string name)
+        {
+
+            Produto produto = _context.Produtos.FirstOrDefault(produto => produto.NomeProduto == name);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(_context.Produtos.ToList());
+        }
+
+        //PUT: api/produto/update
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update([FromBody] Produto produto)
+        {
+            _context.Produtos.Update(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
 
 
-   
+    }
+
+
 }
